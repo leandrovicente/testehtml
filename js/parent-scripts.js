77edadcf6631fa0a2d23bfeae83af47e88dab853
Leandro Vicente <leandro.vicente98@outlook.com>
2024-02-27 17:33:58 -0300

    feat: create application 2buy-tracker

--- js/parent-scripts.js
+++ js/parent-scripts.js
@@ -1,4 +1,33 @@
-(function(){
+
+(function () {
+   (function (w, d, s, l, i) {
+     w[l] = w[l] || [];
+     w[l].push({
+       "gtm.start": new Date().getTime(),
+       event: "gtm.js",
+     });
+     var f = d.getElementsByTagName(s)[0],
+       j = d.createElement(s),
+       dl = l != "dataLayer" ? "&l=" + l : "";
+     j.async = true;
+     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+     f.parentNode.insertBefore(j, f);
+   })(window, document, "script", "dataLayer", "GTM-PT45X2P");
+  
+   (function (w, d, s, l, i) {
+     w[l] = w[l] || [];
+     w[l].push({
+       "gtm.start": new Date().getTime(),
+       event: "gtm.js",
+     });
+     var f = d.getElementsByTagName(s)[0],
+       j = d.createElement(s),
+       dl = l != "dataLayer" ? "&l=" + l : "";
+     j.async = true;
+     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+     f.parentNode.insertBefore(j, f);
+   })(window, document, "script", "dataLayer", "GTM-NQGV93X3");
+  
   try {
     var receiveMessage = function(event) {
       try {
